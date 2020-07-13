1396efd69f4d21814eba42f360b96f9185f72266
Guilherme Martins <guiribeiro63@hotmail.com>
2020-07-13 10:29:32 -0300

    Initial commit

---
+++ ToBeHeroProject/backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const routes = express.Router();
+
+const SessionController = require('./controllers/SessionController');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+
+routes.post('/session', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
